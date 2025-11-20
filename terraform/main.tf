a10751ef635a867b33f49c1c4ccef847e381ff1c
AartiPardeshi23 <137265902+AartiPardeshi23@users.noreply.github.com>
2025-11-20 21:57:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# -----------------------------------
+# Resource Group
+# -----------------------------------
 resource "azurerm_resource_group" "rg" {
   name     = "tf-demo-rg"
   location = "Central India"
 }
 
+# -----------------------------------
+# Virtual Network
+# -----------------------------------
 resource "azurerm_virtual_network" "vnet" {
   name                = "tf-demo-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -10,20 +29,9 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "subnet" {
-  name     = "tf-demo-subnet"
-  resource "azurerm_resource_group" "rg" 
-  name     = "tf-demo-rg"
-  location = "Central India"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "tf-demo-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
+# -----------------------------------
+# Subnet
+# -----------------------------------
 resource "azurerm_subnet" "subnet" {
   name                 = "tf-demo-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -31,6 +39,19 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+# -----------------------------------
+# Public IP
+# -----------------------------------
+resource "azurerm_public_ip" "public_ip" {
+  name                = "tf-demo-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# -----------------------------------
+# NIC
+# -----------------------------------
 resource "azurerm_network_interface" "nic" {
   name                = "tf-demo-nic"
   location            = azurerm_resource_group.rg.location
@@ -44,62 +65,9 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_public_ip" "public_ip" {
-  name                = "tf-demo-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "tf-demo-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = var.admin_username
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = var.ssh_public_key
-  }
-  custom_data = file("${path.module}/cloudinit.yaml")
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
-    version   = "latest"
-  }
-}
-
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "tf-demo-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "tf-demo-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
-  }
-}
-
+# -----------------------------------
+# Linux VM
+# -----------------------------------
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "tf-demo-vm"
   resource_group_name = azurerm_resource_group.rg.name
@@ -124,12 +92,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   custom_data = base64encode(file("${path.module}/cloudinit.yaml"))
 
-
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-focal"
     sku       = "20_04-lts"
     version   = "latest"
   }
-
 }
